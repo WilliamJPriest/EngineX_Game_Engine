9afd09f2f5a1b97c1ff92fecdb149d29f07a6673
brookehorizon <danieltashman17@gmail.com>
2022-04-28 15:55:57 +0300

    updated

--- demo.py
+++ demo.py
@@ -3,8 +3,8 @@ from Editor import obj, Editor_window
 
 
 e = Editor_window.Window()
-e.Win2D()
+e.Win()
 
-obj.add_2dobj(Model='model/scene.gltf')
+obj.add_obj(Model='model/scene.gltf')
 Editor_window.win.run()
 
