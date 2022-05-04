232e3e9ae75414e82c9533d0751f37b913b5ec89
brookehorizon <danieltashman17@gmail.com>
2022-05-04 21:12:44 +0300

    updated

--- demo.py
+++ demo.py
@@ -1,9 +1,8 @@
 from Editor import obj, Editor_window
+from multiprocessing import Process
 
 
 
 e = Editor_window.Window()
-e.Win()
-
-obj.add_obj(Model='freeModels/model/scene.gltf')
+e.Win2D()
 Editor_window.win.run()
