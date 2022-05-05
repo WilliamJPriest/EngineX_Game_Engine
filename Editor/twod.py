1925237478a5cd0eb1f39f997638d61ea5bea33e
brookehorizon <danieltashman17@gmail.com>
2022-05-06 01:48:12 +0300

    updated

--- Editor/twod.py
+++ Editor/twod.py
@@ -1,37 +1,18 @@
 from ursina import *
 import dearpygui.dearpygui as dpg
 from Editor import obj
+from Editor.ui.main import *
 
 
 
-class TwoD(Button):
-    obj = []
+class TwoD(Button, Main):
     def __init__(self):
         super().__init__(model='', scale=0)
-
-    def Create_obj(self):
-        def test():
-            obj.add_obj(Model=dpg.get_value('model'))
-        with dpg.window(label="Create object", height=300, width=300, pos=(100,0)):
-            dpg.add_text("")
-            dpg.add_input_text(label="Name", default_value="A10", tag='name')
-            dpg.add_input_text(label="Model", default_value="freeModels/model/scene.gltf", tag='model')
-            dpg.add_button(label="Create", callback=test)
-
-    def btns(self):
-        dpg.add_button(label='new object', callback=self.Create_obj, width=200, height=55)
+        Main.__init__(self, obj)
 
     def input(self, key):
         if key == 'm':
-            dpg.create_context()
-            with dpg.window(label="EngineX", height=600, width=600):
-                dpg.add_text("")
-                self.btns()
-            dpg.create_viewport(title='EngineX', width=600, height=600)
-            dpg.setup_dearpygui()
-            dpg.show_viewport()
-            dpg.start_dearpygui()
-            dpg.destroy_context()
+            self.run_main()
         if key == 'd':
             camera.rotation_y += 1
         if key == 'a':
