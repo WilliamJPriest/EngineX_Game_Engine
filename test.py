76e5dd53e23dfcc16bf36bb299ca0be14c9af3c9
brookehorizon <danieltashman17@gmail.com>
2022-05-06 01:48:02 +0300

    updated

--- test.py
+++ test.py
@@ -1,25 +1,8 @@
-import dearpygui.dearpygui as dpg
-
-class Main_WIN:
-    def Create_obj(self):
-        with dpg.window(label="Create object", height=300, width=300, pos=(100,0)):
-            dpg.add_text("")
-            dpg.add_input_text(label="Model", default_value="cube", tag='model')
-            dpg.add_button(label="Create", callback=self.val)
-
-    def run(self):
-        dpg.create_context()
-        with dpg.window(label="EngineX", height=600, width=600):
-            dpg.add_slider_float(label="Ground", default_value=1, max_value=100)
-            dpg.add_text("")
-            dpg.add_button(label='new object', callback=self.Create_obj)
-
-        dpg.create_viewport(title='Custom Title', width=600, height=600)
-        dpg.setup_dearpygui()
-        dpg.show_viewport()
-        dpg.start_dearpygui()
-        dpg.destroy_context()
-
-Main_WIN().run()
+from Editor import obj, Editor_window
+from multiprocessing import Process
 
 
+
+e = Editor_window.Window()
+e.Win2D()
+Editor_window.win.run()
