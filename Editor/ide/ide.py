48963ef37de9ac2c2dfd108c25235a79ea18dffa
brookehorizon <danieltashman17@gmail.com>
2022-05-23 18:29:04 +0300

    added

---
+++ Editor/ide/ide.py
@@ -0,0 +1,27 @@
+from tkinter import *
+from turtle import bgcolor, color
+
+
+
+class IDE:
+    def __init__(self, title, **kwargs):
+        self.title = title
+
+    def run(self):
+        code = self.editor.get('1.0', END)
+        exec(code)
+
+    def run_ide(self):
+        compiler = Tk()
+        compiler.title(self.title)
+        menu_bar = Menu(compiler)
+        run_bar = Menu(menu_bar)
+        run_bar.add_command(label='Run', command=self.run)
+        menu_bar.add_cascade(label='Run', menu=run_bar)
+        compiler.config(menu=menu_bar)
+        self.editor = Text(bg='darkblue',fg='white')
+        self.editor.pack()
+        compiler.mainloop()
+
+if __name__ == '__main__':
+    IDE('Add Script').run_ide()
