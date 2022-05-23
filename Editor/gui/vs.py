f1f022b61f3463216963fa06f9f021facad4a783
brookehorizon <danieltashman17@gmail.com>
2022-05-23 16:38:14 +0300

    updated vs.py

--- Editor/gui/vs.py
+++ Editor/gui/vs.py
@@ -5,9 +5,11 @@ class Object(Error_Window):
     key = None
     def link_callback(self, sender, app_data):
         dpg.add_node_link(app_data[0], app_data[1], parent=sender)
-        while self.key != 'q':
-            if self.key == dpg.get_value('onclick'):
-                self.change()
+
+        if dpg.get_value('onclick'):
+            while self.key != 'q':
+                if self.key == dpg.get_value('onclick'):
+                    self.change()
 
     def delink_callback(self, sender, app_data):
         dpg.delete_item(app_data)
@@ -35,7 +37,7 @@ class Object(Error_Window):
         except Exception as e:
             self.err_win(dpg, e)
             
-    def create_node(self, onclick='c'):
+    def create_node(self, onclick=''):
         with dpg.node_editor(callback=self.link_callback, delink_callback=self.delink_callback):
             with dpg.node(label=self.model):
                 with dpg.node_attribute(label="Node A4"):
@@ -48,10 +50,16 @@ class Object(Error_Window):
                 with dpg.node_attribute(label="Node A1", attribute_type=dpg.mvNode_Attr_Output):
                     dpg.add_input_text(label="onClick", width=50, default_value=onclick, tag='onclick')
 
+            with dpg.node(label="OnCollision", pos=(400, 0)):
+                with dpg.node_attribute(label="Node A1", attribute_type=dpg.mvNode_Attr_Output):
+                    ''
+
+
     def object(self):
         dpg.create_context()
         with dpg.window(label=f"Object {self.model}", width=1000, height=500):
             self.create_node()
+            
                 
                         
     def run_vs(self):
