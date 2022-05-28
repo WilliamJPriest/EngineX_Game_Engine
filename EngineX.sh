66273fa07811c2342a1b4f115739e452d87a4535
brookehorizon <danieltashman17@gmail.com>
2022-05-28 16:36:39 +0300

    updated

--- EngineX.sh
+++ EngineX.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+
+pip install -r requirements.txt
 zenity --info --title="Installation complete" --text="Installation complete!" --no-wrap
 notify-send "Starting EngineX Hub"
 python3 ./hub
