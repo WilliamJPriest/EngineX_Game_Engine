92110068abbb3f820ec244c3da6cde15749a63d5
brookehorizon <danieltashman17@gmail.com>
2022-06-04 22:11:32 +0300

    Updated

--- NewProject/__main__.py
+++ NewProject/__main__.py
@@ -13,6 +13,6 @@ def Newproject_gui():
     
 
 def Newproject(project):
-    os.system(f'bash  NewProject/NewProject.sh {project}')
+    os.system(f'cp demo.py {project}')
 
 Newproject(sys.argv[1])
