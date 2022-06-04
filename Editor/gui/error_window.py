75c219283bfb9351dbb45990cff5ac3127567361
brookehorizon <danieltashman17@gmail.com>
2022-06-04 22:11:42 +0300

    Updated

--- Editor/gui/error_window.py
+++ Editor/gui/error_window.py
@@ -10,6 +10,7 @@ class Error_Window:
 class Error_Window_Tk:
     def err_win_tk(self, err):
         compiler = tkinter.Tk()
+        compiler.title(err)
         text = tkinter.Label(compiler, text=f'{err}', width=40)
         text.pack()
         compiler.mainloop()
