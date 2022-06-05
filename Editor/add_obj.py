252ce794c0630cfc0ac455b434987a4d5f402c82
brookehorizon <danieltashman17@gmail.com>
2022-06-05 13:40:03 +0300

    updated

--- Editor/add_obj.py
+++ Editor/add_obj.py
@@ -1,10 +1,40 @@
+import  json
+from Editor.gui.error_window import *
+
+
+objects = {}
+
 class Add_obj:
 
-    objects = {}
+    def objects(self) -> dict:
+        try:
+            f = open('objects.txt', 'r').read()
+            y = json.loads(f)
+            return y
+        except Exception as e:
+            Error_Window_Tk().err_win_tk(e)
+            self.add(Model='cube', Position=(5, 2, 5), Name='Cube')
+            self.objects()
 
-    def add(self, Model, Position:tuple, Color, Name, **kwargs):
-        self.objects[Name] = {
+    def add(self, Model, Position:tuple, Color='', Name='', **kwargs):
+        objects[Name] = {
         'model':Model, 
         'color':Color,
         'pos':tuple(int(i) for i in Position)
         }
+        
+        y = json.dumps(objects)
+        f = open("objects.txt", "w")
+        f.write(y)
+        f.close()
+
+    def test(self):
+        self.add(Model='cube', Position=(5, 2, 5), Name='test')
+        self.add(Model='cube 1', Position=(5, 2, 5), Name='test 1')
+        for k, v in self.objects().items():
+            print(v['model'])
+
+
+
+    
+
