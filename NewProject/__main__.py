04d6c407d2a8ef4bb276398f3104d7fc52ecdf32
EmmaHayes-coder <77508994+EmmaHayes-coder@users.noreply.github.com>
2022-06-09 20:05:21 +0300

    Fixed

--- NewProject/__main__.py
+++ NewProject/__main__.py
@@ -1,16 +1,8 @@
 from subprocess import check_output
-from New import *
 import os
 import sys
 
-def Newproject_gui():
-    app = QtWidgets.QApplication(sys.argv)
-    window = QtWidgets.QMainWindow()
-    demo = LoadingGif()
-    demo.mainUI(window)
-    window.show()
-    sys.exit(sys.argv[1] in check_output(['ls']).decode())
-    
+
 
 def Newproject(project):
     os.system(f'cp demo.py {project}')
