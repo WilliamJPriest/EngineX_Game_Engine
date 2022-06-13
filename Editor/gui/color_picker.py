2f988ef087d82f52e1e3a38e64a4e44c6e7fd0cf
brookehorizon <danieltashman17@gmail.com>
2022-06-13 16:52:33 +0300

    Removed

--- Editor/gui/color_picker.py
+++
@@ -1,5 +0,0 @@
-class Color_Picker:
-    def color_picker(self, dpg):
-        with dpg.window(label="Color"):
-            dpg.add_color_picker(tag='Color')
-            print(dpg.get_value('Color'))
