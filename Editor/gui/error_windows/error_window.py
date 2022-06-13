e94dd2ba00d34499c2c399efd1fb3342e6192df5
brookehorizon <danieltashman17@gmail.com>
2022-06-13 16:52:02 +0300

    Added

---
+++ Editor/gui/error_windows/error_window.py
@@ -0,0 +1,9 @@
+
+
+class Error_Window:
+    def err_win(self, dpg, err):
+        with dpg.window(label="Error"):
+            dpg.add_text(err)
+            with dpg.popup(dpg.last_item(), mousebutton=dpg.mvMouseButton_Left, modal=True, tag="modal_id"):
+                dpg.add_button(label="Close", callback=lambda: dpg.configure_item("modal_id", show=False))
+
