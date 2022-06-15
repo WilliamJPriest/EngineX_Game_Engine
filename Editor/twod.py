ebb606eb6edba16f2345af3c3f385d577efaf40b
brookehorizon <danieltashman17@gmail.com>
2022-06-15 15:02:05 +0300

    updated

--- Editor/twod.py
+++ Editor/twod.py
@@ -8,11 +8,16 @@ from ursina.prefabs.first_person_controller import FirstPersonController
 
 class TwoD(Button, Main, Error_Window, obj.Render):
     def __init__(self):
-        self.render_all()
+        self.render_all(running=self.running)
         super().__init__(model='', scale=0)
         Main.__init__(self, obj)
 
+    key = None
+    running = False
+    
     def input(self, key):
+        self.key = key
+
         
         if key == 'm':
             run_main = threading.Thread(target=self.run_main)
@@ -30,12 +35,20 @@ class TwoD(Button, Main, Error_Window, obj.Render):
             camera.rotation_x -= 5
             
         if key == 'r':
-            self.Run()
+            self.running = True
+
+        if key == 'q':
+            self.running = False
 
         if key == 'l':
             self.render_all()
 
         if key == '3':
             FirstPersonController()
+
         if key == '2':
             FirstPersonController().disable()
+
+        if self.running:
+            print(self.running)
+            self.OnClick(key, self.running)
