43f91445c92f015892e7adf5fb02aab72597747b
brookehorizon <danieltashman17@gmail.com>
2022-06-16 16:52:55 +0300

    updated

--- Editor/obj.py
+++ Editor/obj.py
@@ -8,6 +8,7 @@ class render_obj(Button, Object, Keys):
     def __init__(self, running, Scale=(1, 1, 1), Origin: float=0.6,Texture: str='', Position:tuple = (5, 2, 5), Name='',Model: str = 'cube', Color='red', **kwargs):
         self.Name = Name
         self.running = running
+        self.Model = Model
         super().__init__(
             parent=scene,
             scale=Scale,
@@ -26,11 +27,19 @@ class render_obj(Button, Object, Keys):
             if self.running == False:
                 self.Mov_Keys(key)
                 self.GUI_Keys(key)
+
+
+class render_obj_play(render_obj):
+    def input(self, key):
+        if self.hovered:
+            b = Text(scale=1, text=f'{self.get_position()} {self.scale}')
+            b.fade_out()
+            self.key = key
             
 
 
 class Render(Add_obj):
-    def render_all(self, running, **kwargs):
+    def render_all(self, running=False, **kwargs):
         for k, v in self.objects().items():
             render_obj(Name=k, running=running, Model=v['model'], Color=v['color'], Position=v['pos'], Scale=v['scale'], Origin=v['origin'])
     
