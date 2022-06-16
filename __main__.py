5ed982f52ca281e0a0603d9295ef831c8c290dbd
brookehorizon <danieltashman17@gmail.com>
2022-06-16 16:58:45 +0300

    updated

--- __main__.py
+++ __main__.py
@@ -1,3 +1,9 @@
-import os
 
-os.system("python3 EngineX/demo.py")
+if __name__ == '__main__':
+    import os
+    from Editor.gui.error_windows import Error_Window_Tk
+
+    try:
+        os.system("python3 EngineX/demo.py")
+    except Exception as e:
+        Error_Window_Tk(e)
