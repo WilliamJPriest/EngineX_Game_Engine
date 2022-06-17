16ac7f222750b470cfb668c8b1d2eb4b618672b1
brookehorizon <danieltashman17@gmail.com>
2022-06-17 19:33:35 +0300

    Updated

--- Editor/project.py
+++ Editor/project.py
@@ -5,5 +5,9 @@ objectsFile = './objects.json'
 # project python file
 projectFile = 'demo.py'
 
+
+# history file
+historyFile = 'history.txt'
+
 # on_click json file
 on_clickFile = './on_click.json'
