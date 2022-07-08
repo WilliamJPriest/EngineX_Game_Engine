2e36d8d11f28aa68600c709f75048e6da72ee37d
brookehorizon <danieltashman17@gmail.com>
2022-07-09 00:54:03 +0300

    Updated

--- Editor/twod.py
+++ Editor/twod.py
@@ -8,48 +8,46 @@ from ursina.prefabs.first_person_controller import FirstPersonController
 
 class TwoD(Button, Main, Error_Window, obj.Render):
     def __init__(self):
-        self.render_all(running=self.running)
+        self.render_all()
         super().__init__(model='', scale=0)
         Main.__init__(self, obj)
 
     key = None
-    running = False
     
     def input(self, key):
         self.key = key
 
-        
-        if key == 'm':
-            run_main = threading.Thread(target=self.run_main)
-            run_main.start()
-            
-        if key == 'd':
-            camera.rotation_y += 5
-            
-        if key == 'a':
-            camera.rotation_y -= 5
-            
-        if key == 's':
-            camera.rotation_x += 5
-            
-        if key == 'w':
-            camera.rotation_x -= 5
-            
-        if key == 'r':
-            self.running = True
+        if not Play:
+            if key == 'm':
+                run_main = threading.Thread(target=self.run_main)
+                run_main.start()
+                
+            if key == 'd':
+                camera.rotation_y += 5
+                
+            if key == 'a':
+                camera.rotation_y -= 5
+                
+            if key == 's':
+                camera.rotation_x += 5
+                
+            if key == 'w':
+                camera.rotation_x -= 5
+                
+            if key == 'r':
+                self.running = True
 
-        if key == 'q':
-            self.running = False
+            if key == 'q':
+                self.running = False
 
-        if key == 'l':
-            self.render_all()
+            if key == 'l':
+                self.render_all()
 
-        if key == '3':
-            FirstPersonController()
+            if key == '3':
+                FirstPersonController()
 
-        if key == '2':
-            FirstPersonController().disable()
+            if key == '2':
+                FirstPersonController().disable()
 
-        if self.running:
-            print(self.running)
-            self.OnClick(key, self.running)
+        else:
+            self.OnClick(key)
