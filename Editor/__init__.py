489ab3b6866e24ac54286cb6b3032b484de2c3a5
brookehorizon <danieltashman17@gmail.com>
2022-07-09 00:54:24 +0300

    Updated

--- Editor/__init__.py
+++ Editor/__init__.py
@@ -1,10 +1,21 @@
 from .project import *
 from datetime import date
+from Editor.gui.error_windows import Error_Window_Tk
 
 today = date.today()
 
-f = open('history.txt', 'r').read()
-history = f + '\n' + projectFile + ' ' + str(today)
-f = open('history.txt', "w")
-f.write(history)
-f.close()
+def history(filename):
+    f = open(filename, 'r').read()
+    history = f + '\n' + projectFile + ' ' + str(today)
+    f = open(filename, "w")
+    f.write(history)
+    f.close()
+
+try:
+    history(historyFile)
+except Exception as e:
+    Error_Window_Tk().err_win_tk(e)
+    exit()
+
+
+    
