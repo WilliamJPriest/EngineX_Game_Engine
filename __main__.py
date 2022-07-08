0b376b5c14e2dc41317b57d139c5f690e314fa74
brookehorizon <danieltashman17@gmail.com>
2022-07-09 00:53:41 +0300

    Updated

--- __main__.py
+++ __main__.py
@@ -1,9 +1,9 @@
-
+from Editor.project import *
 if __name__ == '__main__':
     import os
     from Editor.gui.error_windows import Error_Window_Tk
 
     try:
-        os.system("python3 EngineX/demo.py")
+        os.system(f"python3 EngineX/{projectFile}")
     except Exception as e:
         Error_Window_Tk(e)
