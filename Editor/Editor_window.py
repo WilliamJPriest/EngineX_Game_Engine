4c7b575ac390df3502b688ab6ba451c7aada49b3
brookehorizon <danieltashman17@gmail.com>
2022-08-10 18:23:36 +0300

    Updated

--- Editor/Editor_window.py
+++ Editor/Editor_window.py
@@ -4,19 +4,19 @@ from Editor.twod import TwoD
 win = Ursina()
 
 class Window:
-    def Win3D(self):
-        for x in range(20):
-            for z in range(20):
-                Entity(
-                    model='cube', 
-                    color=color.dark_gray, 
-                    collider='box',
-                    ignore=True,
-                    position= (x, 0, z), 
-                    origin_y=0.5, 
-                    parent=scene,
-                    texture = 'white_cube',CollisionBox=True)
-        FirstPersonController()
+    # def Win3D(self):
+    #     for x in range(20):
+    #         for z in range(20):
+    #             Entity(
+    #                 model='cube', 
+    #                 color=color.dark_gray, 
+    #                 collider='box',
+    #                 ignore=True,
+    #                 position= (x, 0, z), 
+    #                 origin_y=0.5, 
+    #                 parent=scene,
+    #                 texture = 'white_cube',CollisionBox=True)
+    #     FirstPersonController()
 
     def Win2D(self):
         for x in range(20):
