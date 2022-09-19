a0c1668c55778370aec29d630551b296349ef131
brookehorizon <danieltashman17@gmail.com>
2022-09-19 12:25:15 +0300

    Updated

--- hub/__main__.py
+++ hub/__main__.py
@@ -2,10 +2,13 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from Hub import *
 import sys
 
-app = QtWidgets.QApplication(sys.argv)
-app.setStyleSheet(open('hub/style.css', 'r').read())
-MainWindow = QtWidgets.QMainWindow()
-ui = Ui_MainWindow()
-ui.setupUi(MainWindow)
-MainWindow.show()
-sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    app.setStyleSheet(open('hub/style.css', 'r').read())
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
